25e20d29159dc2b27118f21dd82fc1ce65fb72cc
Bethel Amadi <amadibethel@icloud.com>
2025-07-15 14:08:26 +0200

    Add updated files to React Project

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Simple Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
