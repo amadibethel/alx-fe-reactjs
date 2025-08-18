b98bedb89ef0434bf15c88db44b24cf1b6f15039
Bethel Amadi <amadibethel@icloud.com>
2025-08-19 01:40:25 +0200

    Add updated file to React Router

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from 'react-router-dom';
+
+// Simulate authentication
+const isAuthenticated = () => localStorage.getItem('auth') === 'true';
+
+export default function ProtectedRoute({ children }) {
+  if (!isAuthenticated()) return <Navigate to="/login" replace />;
+  return children;
+}
