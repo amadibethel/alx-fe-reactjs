8ac3330aa94b2e7c171e97c58806f53743dfff09
Bethel Amadi <amadibethel@icloud.com>
2025-08-19 01:31:50 +0200

    Add updated file to React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 export default function Profile() {
   return (
@@ -7,8 +9,12 @@ export default function Profile() {
       <nav style={{ marginBottom: 12 }}>
         <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
-      {/* Nested route outlet */}
-      <Outlet />
+
+      {/* Nested routes defined here */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
