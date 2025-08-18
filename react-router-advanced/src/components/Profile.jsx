57041d08d8dc094374894fc54c4427b4d195f19e
Bethel Amadi <amadibethel@icloud.com>
2025-08-19 01:26:52 +0200

    Add updated files to React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from 'react-router-dom';
+
 export default function Profile() {
   return (
     <div>
       <h1>Profile Page</h1>
-      <nav>
+      <nav style={{ marginBottom: 12 }}>
         <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
-      <Outlet /> {/* Nested routes rendered here */}
+      {/* Nested route outlet */}
+      <Outlet />
     </div>
   );
 }
